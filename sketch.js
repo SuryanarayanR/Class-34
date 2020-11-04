e25579b96600f7ab8551af35bdf4451077ff7ca5
Suryanarayan <suryanarayanrnair@gmail.com>
2020-11-04 17:10:52 +0530

    Database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
-var ball;
+var ball, db, position;
 
 function setup(){
     createCanvas(500,500);
+
+    db = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+ 
+    var location = db.ref("ball/position");
+    location.on("value",read,showerr);
+
 }
 
 function draw(){
@@ -23,7 +30,31 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function changePosition(X, Y){
+    db.ref("ball/position").set({
+        x : position.x + X,
+        y : position.y + Y
+    });
+    
 }
+
+function read(rd){
+    position = rd.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showerr(){
+    console.log("Error")
+}
+
+
+/*
+.ref() - Refers to the location of the database value we want
+
+.on() - READ --> creates a listener which keeps listening to the changes in the database
+        1. To read/store from the database
+        2. To show us error if there is any problem in database
+
+.set() - WRITE --> update/change the database value
+*/
